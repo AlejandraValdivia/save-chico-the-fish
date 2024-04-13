0c785499f6355888b5e3434d7268a31d5513af20
Alejandra Valdivia <alexvcodes@gmail.com>
2024-04-13 15:11:57 -0700

    feat: added grid template, play and pause music buttons and global variables

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
-// const chicoFish = document.getElementById('chicofish');
-// chicoFish.style.position = 'absolute';
-// chicoFish.style.display = 'block';
-// console.log(chicoFish);
+// Global DOM manipulation / variables
+const game = document.getElementById('game');
+const movement = document.getElementById('movement');
+const score = document.getElementById('score');
+const status = document.getElementById('status');
+const ctx = game.getContext('2d');
+let chicoCharacter;
+let shark;
+const fishImg = document.getElementById('chico-fish');
+fishImg.style.position = 'absolute';
+fishImg.style.display = 'block';
+console.log(fishImg);
+const sharkImg = document.getElementById('shark');
+
+// ====================== PAINT INTIAL SCREEN ======================= //
+// EVENT LISTENERS
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('DOMContentLoaded');
+    
+});
